0702c398d8fab67aa7c7c52f6b381c2ccb698fba
roger <>
2017-08-13 17:58:30 +0200

    Added ResourceManager class

---
+++ src/ResourceManager.cpp
@@ -0,0 +1,9 @@
+//
+//  ResourceManager.cpp
+//  clip
+//
+//  Created by roger on 13/08/2017.
+//
+//
+
+#include "ResourceManager.h"
---
+++ src/ResourceManager.h
@@ -0,0 +1,24 @@
+//
+//  ResourceManager.hpp
+//  clip
+//
+//  Created by roger on 13/08/2017.
+//
+//  A class to manage resources.
+//
+//  It holds a list of names resources and takes care of everything that
+//  concerns them, such as preloding files, ...
+
+#ifndef ResourceManager_h
+#define ResourceManager_h
+
+#include "ofMain.h"
+
+namespace loopier {
+    class ResourceManager
+    {
+        
+    };
+}
+
+#endif /* ResourceManager_hpp */
