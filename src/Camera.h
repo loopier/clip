b2fb98d07195a9e2a28ae3094215edd7a44f2eb6
roger <rogerpibernat@gmail.com>
2017-09-26 11:05:16 +0200

    Added Camera class

---
+++ src/Camera.h
@@ -0,0 +1,38 @@
+//
+//  Camera.h
+//  clip
+//
+//  Created by roger on 26/09/2017.
+//  Copyright 2017 Loopier. All rights reserved.
+//
+//  Manages 
+
+#ifndef _Camera
+#define _Camera
+
+
+#include "ofMain.h"
+
+#inculde "ofVideoGrabber.h"
+
+
+class Camera{
+	
+public:
+    
+    void setup();
+    void update();
+    void draw();
+    void exit();
+	void keyPressed  (int key);
+	void keyReleased  (int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased();
+    
+private:
+    ofVideoGrabber cam;
+};
+
+#endif
