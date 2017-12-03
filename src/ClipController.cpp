8fecbf99b77d104f136c57400ca3fea89dd6ec45
roger <rogerpibernat@gmail.com>
2017-12-03 17:23:29 +0100

    Added ClipController.h/cpp

---
+++ src/ClipController.cpp
@@ -0,0 +1,9 @@
+//
+//  ClipController.cpp
+//  clip
+//
+//  Created by roger on 03/12/2017.
+//  Copyright 2017 __MyCompanyName__. All rights reserved.
+//
+
+#include "ClipController.h"
---
+++ src/ClipController.h
@@ -0,0 +1,18 @@
+//
+//  ClipController.h
+//  clip
+//
+//  Created by roger on 03/12/2017.
+//  Copyright 2017 __MyCompanyName__. All rights reserved.
+//
+//  A collection of functions to manage clips.  This is the control center for managing everything
+
+#ifndef _ClipController
+#define _ClipController
+
+
+#include "ofMain.h"
+#include "Clip.h"
+
+
+#endif
