2dcbd4dcc95754c5cc0ab474426e65220e0371db
roger <rogerpibernat@gmail.com>
2019-01-22 14:59:15 +0100

    Remove docopt example

--- livecoding/clipcli.py
+++ livecoding/clipcli.py
@@ -18,38 +18,6 @@ import os
 import sys
 import cmd
 from cliplang.cliplang import *
-# from docopt import docopt, DocoptExit
-#
-#
-# def docopt_cmd(func):
-#     """
-#     This decorator is used to simplify the try/except block and pass the result
-#     of the docopt parsing to the called action.
-#     """
-#     def fn(self, arg):
-#         try:
-#             opt = docopt(fn.__doc__, arg)
-#
-#         except DocoptExit as e:
-#             # The DocoptExit is thrown when the args do not match.
-#             # We print a message to the user and the usage block.
-#
-#             print('Invalid Command!')
-#             print(e)
-#             return
-#
-#         except SystemExit:
-#             # The SystemExit exception prints the usage for --help
-#             # We do not need to do the print here.
-#
-#             return
-#
-#         return func(self, opt)
-#
-#     fn.__name__ = func.__name__
-#     fn.__doc__ = func.__doc__
-#     fn.__dict__.update(func.__dict__)
-#     return fn
 
 
 class ClipCli (cmd.Cmd):
@@ -75,25 +43,9 @@ class ClipCli (cmd.Cmd):
         """Parses the arguments to convert them to clip commands"""
         print(arg)
 
-    # @docopt_cmd
     def do_print(self, arg):
         """Usage: print <msg>"""
         print(str(type(arg))+" "+arg)
-
-#     @docopt_cmd
-#     def do_tcp(self, arg):
-#         """Usage: tcp <host> <port> [--timeout=<seconds>]"""
-#
-#         print(arg)
-#
-#     @docopt_cmd
-#     def do_serial(self, arg):
-#         """Usage: serial <port> [--baud=<n>] [--timeout=<seconds>]
-# Options:
-#     --baud=<n>  Baudrate [default: 9600]
-#         """
-#
-#         print(arg)
 
     def do_shell(self, arg):
         """Execute any shell command.  You may use ! instead.
@@ -123,10 +75,6 @@ Usage: (shell | !) <command>"""
         self.parse_cmd(arg)
         print("Default: {}".format(arg))
 
-# opt = docopt(__doc__, sys.argv[1:])
-
-# if opt['--interactive']:
-    # ClipCli().cmdloop()
 ClipCli().cmdloop()
 
 print(opt)
